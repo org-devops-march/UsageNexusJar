c33284371a2e919be8f04cf2bf78732df612451e
Surendra <51156858+surendradire2@users.noreply.github.com>
2022-03-22 09:41:49 +0100

    first commit

---
+++ src/main/java/grouipid-usagenexusJar/App.java
@@ -0,0 +1,13 @@
+package grouipid-usagenexusJar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
